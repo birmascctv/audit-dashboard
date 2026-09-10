80cc4993f941ed8cfed8db08bf37468eb6ec8630
birmascctv <birmascctv@example.com>
2026-09-10 14:30:45 +0700

    Sidebar typo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,6 +9,6 @@
 </template>
 
 <script setup>
-import Sidebar from './components/Sidebar.vue'
+import Sidebar from './components/SideBar.vue'
 import Dashboard from './pages/Dashboard.vue'
 </script>
